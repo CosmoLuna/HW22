225966d7b1d2a8ae9cdecac4d889ffa67e89700d
Smir_hd87@itstepru.onmicrosoft.com <Smir_hd87@itstepru.onmicrosoft.com>
2022-03-22 12:45:47 +0500

    corrected hw mistakes

--- HW22/HW22.cpp
+++ HW22/HW22.cpp
@@ -5,14 +5,15 @@ void func(int* num, int num1) {
 	int power = 1;
 	for (int i = 0; i < num1; i++)
 		power *= *num;
-	cout << power;
+	*num = power;
+	cout << *num << endl;
 }
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 
 	//Задача 1
-	int array[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	/*int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	int* pArray = array;
 	cout << "Задача1\nИзначальный массив:\n[";
 	for (int i = 0; i < 10; i++)
@@ -26,13 +27,13 @@ int main() {
 	cout << "Итоговый массив:\n[";
 		for (int i = 0; i < 10; i++)
 			cout << *(pArray + i) << ", ";
-	cout << "\b\b]\n\n";
+	cout << "\b\b]\n\n";*/
 
 	// Задача 2
 	cout << "Задача 2\n";
-	int n = 10;
+	int n = 4;
 	func(&n, 3);
-
+	cout << n << endl;
 
 
 	return 0;
